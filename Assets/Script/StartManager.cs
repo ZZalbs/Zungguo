899f40662acc0d9e5a5f1b9a728a179ece05694a
jjjwwer <70564930+jjjwwer@users.noreply.github.com>
2020-12-05 22:26:56 +0900

    qqq

---
+++ Assets/Script/StartManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartManager : MonoBehaviour
+{
+    private void Start()
+    {
+        GameObject.Find("BackGround").GetComponent<ScrollManager>().enabled = false;
+        GameObject.Find("floorManager").GetComponent<ScrollManager>().enabled = false;
+        GameObject.Find("QuizManager").GetComponent<QuizFile>().enabled = false;
+        GameObject.Find("GameUI").transform.GetChild(1).gameObject.SetActive(false);
+        GameObject.Find("GameUI").transform.GetChild(2).gameObject.SetActive(false);
+    }
+
+
+
+
+}
