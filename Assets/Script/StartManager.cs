39b6da2c6d15892b552f9ae1f856be0f0069333c
jjjwwer <70564930+jjjwwer@users.noreply.github.com>
2020-12-05 23:01:02 +0900

    start

--- Assets/Script/StartManager.cs
+++ Assets/Script/StartManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class StartManager : MonoBehaviour
 {
+    bool MouseDown = false;
+    float timeCount = 4;
+    public Sprite[] TimeLeftSprites;
+
     private void Start()
     {
         GameObject.Find("BackGround").GetComponent<ScrollManager>().enabled = false;
@@ -13,7 +17,23 @@ public class StartManager : MonoBehaviour
         GameObject.Find("GameUI").transform.GetChild(2).gameObject.SetActive(false);
     }
 
-    
+    private void Update()
+    {
+        if (Input.touchCount >= 1 || Input.GetMouseButtonDown(0) || MouseDown)
+        {
+            MouseDown = true;
+            timeCount -= Time.deltaTime;
+            GameObject.Find("GameUI").transform.Find("StartMenuUI").Find("TimeLeft").GetComponent<SpriteRenderer>().sprite = TimeLeftSprites[(int)timeCount % 4];
+            if(timeCount <= 0)
+            {
+                GameObject.Find("BackGround").GetComponent<ScrollManager>().enabled = true;
+                GameObject.Find("floorManager").GetComponent<ScrollManager>().enabled = true;
+                GameObject.Find("QuizManager").GetComponent<QuizFile>().enabled = true;
+                GameObject.Find("GameUI").transform.GetChild(1).gameObject.SetActive(true);
+                GameObject.Find("GameUI").transform.GetChild(0).gameObject.SetActive(false);
+            }
+        }
+    }
 
 
 }
